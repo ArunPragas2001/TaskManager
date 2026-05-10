657a6bb1b79de521ea25de4159479a2622e2f87b
Your Name <pragasarun1@gmail.com>
2026-05-10 15:29:56 +0530

    Date format changed

--- public/js/app.js
+++ public/js/app.js
@@ -43,8 +43,8 @@ function renderTasks() {
 
     filteredTasks.forEach(task => {
         const deadlineDate = new Date(task.deadline);
-        const day = deadlineDate.getDate().toString().padStart(2, '0');
-        const month = (deadlineDate.getMonth() + 1).toString().padStart(2, '0');
+        const day = deadlineDate.getDate();
+        const month = deadlineDate.getMonth() + 1;
         const year = deadlineDate.getFullYear();
         const time = deadlineDate.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
         const formattedDate = `${day}/${month}/${year} ${time}`;
